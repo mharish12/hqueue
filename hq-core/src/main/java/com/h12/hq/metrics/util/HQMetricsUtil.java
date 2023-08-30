cecb2d0020aef88575125a8223b22a46316791b7
Harish Madineni <harishmadineni@Harishs-MacBook-Pro.local>
2023-08-31 01:26:22 +0530

    Added metrics for memory.

--- hq-core/src/main/java/com/h12/hq/metrics/util/HQMetricsUtil.java
+++ hq-core/src/main/java/com/h12/hq/metrics/util/HQMetricsUtil.java
@@ -1,20 +1,62 @@
 package com.h12.hq.metrics.util;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.MetricSet;
+import com.codahale.metrics.jvm.*;
 import io.micrometer.core.instrument.*;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.Gauge;
+import io.prometheus.client.dropwizard.DropwizardExports;
+import org.slf4j.LoggerFactory;
+
+import java.lang.management.ManagementFactory;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class HQMetricsUtil {
     private static final CollectorRegistry hqCollectorRegistry = new CollectorRegistry(true);
     //    private static final CollectorRegistry hqCollectorRegistry = CollectorRegistry.defaultRegistry;
     public static final PrometheusMeterRegistry hqPrometheusRegistry;
+    private static final MetricRegistry metricRegistry;
 
     static {
         hqPrometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT, hqCollectorRegistry, Clock.SYSTEM);
         hqPrometheusRegistry.config().commonTags("application", "hq-application");
         Metrics.addRegistry(hqPrometheusRegistry);
+        metricRegistry = new MetricRegistry();
+//        hqCollectorRegistry.register(new GarbageCollectorMetricSet());
+//        hqCollectorRegistry.register(new CachedThreadStatesGaugeSet());
+//        hqCollectorRegistry.register(new MemoryUsageGaugeSet());
+        hqCollectorRegistry.register(new DropwizardExports(metricRegistry));
+
+        final LoggerContext factory = (LoggerContext) LoggerFactory.getILoggerFactory();
+        final ch.qos.logback.classic.Logger root = factory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+        final ConsoleAppender<ILoggingEvent> metrics = new ConsoleAppender<ILoggingEvent>();
+        metrics.setContext(root.getLoggerContext());
+        metrics.start();
+        root.addAppender(metrics);
+        registerAll("gc", new GarbageCollectorMetricSet(), metricRegistry);
+        registerAll("buffers", new BufferPoolMetricSet(ManagementFactory.getPlatformMBeanServer()), metricRegistry);
+        registerAll("memory", new MemoryUsageGaugeSet(), metricRegistry);
+        registerAll("threads", new ThreadStatesGaugeSet(), metricRegistry);
+        registerAll("cached_thread_states", new CachedThreadStatesGaugeSet(60, TimeUnit.SECONDS), metricRegistry);
+        registerAll("class_loading", new ClassLoadingGaugeSet(), metricRegistry);
+
+    }
+
+    private static void registerAll(String prefix, MetricSet metricSet, MetricRegistry registry) {
+        for (Map.Entry<String, Metric> entry : metricSet.getMetrics().entrySet()) {
+            if (entry.getValue() instanceof MetricSet) {
+                registerAll(prefix + "." + entry.getKey(), (MetricSet) entry.getValue(), registry);
+            } else {
+                registry.register(prefix + "." + entry.getKey(), entry.getValue());
+            }
+        }
     }
 
     public static Counter counter(String name, String... tags) {
--- hq-http/src/main/java/com/h12/hq/server/UndertowServer.java
+++ hq-http/src/main/java/com/h12/hq/server/UndertowServer.java
@@ -8,11 +8,17 @@ import com.h12.hq.server.http.handler.RouteHttpHandler;
 import io.github.classgraph.MethodInfo;
 import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
+import io.undertow.security.api.AuthenticationMechanism;
+import io.undertow.security.handlers.AuthenticationCallHandler;
+import io.undertow.security.handlers.AuthenticationMechanismsHandler;
+import io.undertow.security.impl.BasicAuthenticationMechanism;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.PathHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class UndertowServer extends AbstractServer {
@@ -20,18 +26,29 @@ public class UndertowServer extends AbstractServer {
     private DependencyManager dependencyManager;
     private Undertow undertowServer;
     private AppContext appContext;
+    private PathHandler handler;
 
     @Override
     public void prepare(DependencyManager dependencyManager) {
         this.dependencyManager = dependencyManager;
         this.appContext = dependencyManager.getAppContext();
+        handler = new PathHandler(10);
 
+        // Create a basic authentication mechanism
+        AuthenticationMechanism mechanism = new BasicAuthenticationMechanism("MyRealm");
+
+// Wrap the mechanism with an authentication handler
+        AuthenticationCallHandler authenticationHandler = new AuthenticationCallHandler(handler);
+        List<AuthenticationMechanism> authenticationMechanisms = new ArrayList<>();
+        authenticationMechanisms.add(mechanism);
+// Use AuthenticationMechanismsHandler to handle multiple mechanisms
+        AuthenticationMechanismsHandler authMechanismsHandler = new AuthenticationMechanismsHandler(authenticationMechanisms);
 //        Undertow.ListenerBuilder listenerBuilder = new Undertow.ListenerBuilder();
-
+//        RolesAllowedHandler rolesAllowedHandler = new RolesAllowedHandler(authMechanismsHandler, "admin");
     }
 
-    private HttpHandler registerHandler(DependencyManager dependencyManager) {
-        PathHandler handler = new PathHandler(10);
+    private HttpHandler registerHandlers(DependencyManager dependencyManager) {
+
         handler.addExactPath("/metrics", new HttpMetricsHandler(dependencyManager));
         for (Map.Entry<String, MethodInfo> route : dependencyManager.getAppContext().getRoutes().entrySet()) {
             HttpHandler httpHandler = new RouteHttpHandler(route.getKey(), route.getValue(), dependencyManager);
@@ -51,7 +68,7 @@ public class UndertowServer extends AbstractServer {
         Undertow.Builder builder = Undertow.builder()
                 .setServerOption(UndertowOptions.ENABLE_STATISTICS, true)
                 .setServerOption(UndertowOptions.ALWAYS_SET_DATE, true);
-        builder.addHttpListener(port, host, registerHandler(dependencyManager));
+        builder.addHttpListener(port, host, registerHandlers(dependencyManager));
         undertowServer = builder.build();
         undertowServer.start();
         logger.info("Server started.");
